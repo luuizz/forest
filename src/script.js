48b21a111b3c36ff1c069a8659755233dedf19af
Luiz Ricardo <luu.izz@hotmail.com>
2025-01-11 19:47:35 -0300

    Ajustes finos e adição de lógica

--- src/script.js
+++ src/script.js
@@ -1,3 +1,21 @@
+const video = document.getElementById("video");
+const temperatura = document.getElementById("temperatura");
+const tempo = document.getElementById("tempo");
+const dia = document.getElementById("dia");
+
+const random = Math.floor(Math.random() * 10) + 20;
+temperatura.innerText = `${random}°`;
+tempo.innerHTML = random < 25 ? `🌧️ ${random + 5}%` : `☀️`;
+
+const date = new Date();
+dia.innerText = date
+  .toLocaleDateString("pt-BR", {
+    weekday: "long",
+  })
+  .replace("-feira", "");
+
+video.src = random < 25 ? `./img/video_chuva.mp4` : `./img/video_sol.mp4`;
+
 const mobileButton = document.getElementById("mobile-button");
 const mobileMenu = document.getElementById("mobile-menu");
 
